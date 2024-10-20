f934a8bc2f652e33cf3f6c32cde553108c57fdb6
Supachai Wongmoon <supachai@outlook.com>
2024-10-21 00:35:15 +0700

    Adjust click area to card

--- app/components/utils/ScriptCard.vue
+++ app/components/utils/ScriptCard.vue
@@ -15,39 +15,39 @@ const props = defineProps({
 </script>
 
 <template>
-  <Card class="flex flex-col hover:bg-gray-50">
-    <CardHeader class="flex-grow px-4 pt-4 pb-2">
-      <a href="">
+  <Card class=" hover:bg-gray-50">
+    <a href="" class="flex flex-col h-full focus:ring-2 focus:ring-primary focus:ring-offset-2 rounded-[var(--radius)]">
+      <CardHeader class="flex-grow px-4 pt-4 pb-2">
         <CardTitle class="line-clamp-2 mb-2 text-sm">
           {{ props.title }}
         </CardTitle>
-      </a>
-    </CardHeader>
-    <div class="line-clamp-3 text-xs text-gray-400 px-4">
-      {{ props.description }}
-    </div>
-    <CardFooter
-      class="flex justify-between items-center space-x-4 px-4 pb-4 pt-4 text-gray-500"
-    >
-      <div class="w-full line-clamp-1">
-        <p class="text-xs truncate">
-          @zupaazhai
-        </p>
+      </CardHeader>
+      <div class="line-clamp-3 text-xs text-gray-400 px-4">
+        {{ props.description }}
       </div>
-      <div class="flex space-x-2">
-        <p class="text-xs 0 inline-flex items-end">
-          <Sparkles size="16" class="mr-2" />
-          <span>
-            {{ props.downloads }}
-          </span>
-        </p>
-        <p class="text-xs inline-flex items-end">
-          <Download size="16" class="mr-2" />
-          <span>
-            {{ props.downloads }}
-          </span>
-        </p>
-      </div>
-    </CardFooter>
+      <CardFooter
+        class="flex justify-between items-center space-x-4 px-4 pb-4 pt-4 text-gray-500"
+      >
+        <div class="w-full line-clamp-1">
+          <p class="text-xs truncate">
+            @zupaazhai
+          </p>
+        </div>
+        <div class="flex space-x-2">
+          <p class="text-xs 0 inline-flex items-end">
+            <Sparkles size="16" class="mr-2" />
+            <span>
+              {{ props.downloads }}
+            </span>
+          </p>
+          <p class="text-xs inline-flex items-end">
+            <Download size="16" class="mr-2" />
+            <span>
+              {{ props.downloads }}
+            </span>
+          </p>
+        </div>
+      </CardFooter>
+    </a>
   </Card>
 </template>
