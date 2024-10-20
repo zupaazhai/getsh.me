9de1a6126faa6100401997e58750afd663d44978
Supachai Wongmoon <supachai@outlook.com>
2024-10-20 13:16:40 +0700

    Add page description

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,15 @@
 <script setup>
 import TopBar from '@/components/TopBar.vue'
+
+useHead({
+  title: 'getsh.me',
+  meta: [
+    {
+      name: 'description',
+      content: 'Discover, create, and share powerful bash scripts to automate and boost your workflow',
+    },
+  ],
+})
 </script>
 
 <template>
